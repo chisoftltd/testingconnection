18b958425054c5803ffbdb125716174bae7a3480
chisoftltd <chisoftltd@gmail.com>
2017-10-20 14:53:41 +0100

    further update

--- connectiontesting.php
+++ connectiontesting.php
@@ -10,10 +10,10 @@
 $servername = "109.228.40.47:53306";
 $username = "ccms_test";
 $password = "ccms_test2!%%^";
-$dbname = "councilDB";
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
