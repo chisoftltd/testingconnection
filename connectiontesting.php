ba3297c9af967716ab166c4c39bc0313d3c53520
chisoftltd <chisoftltd@gmail.com>
2017-10-20 13:41:18 +0100

    Initial commit

---
+++ connectiontesting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chisoft
+ * Date: 2017-10-20
+ * Time: 13:19
+ */
+
+
+$servername = "109.228.40.47:53306";
+$username = "ccms_test";
+$password = "ccms_test2!%%^";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chisoft
+ * Date: 2017-10-20
+ * Time: 13:36
+ */
+
+?>
+<HTML>
+<body>
+<h1>hello</h1>
+
+
+
+</body>
+</HTML>
