92268f45d2176366d3deb2bd5edae961c06c6c77
chisoftltd <chisoftltd@gmail.com>
2017-10-20 14:01:09 +0100

    further update

--- index.php
+++ index.php
@@ -5,11 +5,66 @@
  * Date: 2017-10-20
  * Time: 13:36
  */
-
+include_once 'connectiontesting.php';
 ?>
-<HTML>
-<body>
-<h1>hello</h1>
-<a href="connectiontesting.php">connect to server</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- Head-->
+<head>
+    <title>Council Manasgement</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Add css file-->
+    <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"/>-->
+    <link rel="stylesheet" href="css/main-style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body><!-- Body area start-->
+<header>
+    <!-- add top navigational bar using bootstrap-->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navoilweb">
+                    <!--<span class="sr-only">Toggle navigation</span>-->
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Home | Council Manasgement</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navoilweb">
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- check if same user is still same as the active session user and load appropriate menu options -->
+
+                        <li class="active"><a href="index.php">Create</a></li>
+                        <li><a href="menu/about.php">Editing</a></li>
+                        <li><a href="menu/courses.php">Search</a></li>
+                        <li><a href="menu/assessment.php">New Record</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<hr> <!-- draw a line-->
+<section>
+</section><!-- end of section-->
+<hr> <!-- draw a line-->
+<footer>
+    <!-- footer area-->
+    <div>
+        <?php include 'include/footer.php'; ?>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script> <!-- jQuery library -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Latest compiled JavaScript -->
 </body>
-</HTML>
+</html>
